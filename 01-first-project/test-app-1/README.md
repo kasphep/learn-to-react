0aa762927751c1a0ce9cba7a27ea84d5d3fae55d
Palamarchuk Slava <97830414+slapaic@users.noreply.github.com>
2022-09-07 17:12:31 +0300

    Shablon for first project
    Create shablon for first react project "test-app-1"

---
+++ 01-first-project/test-app-1/README.md
@@ -0,0 +1,1 @@
+## First test-app project
---
+++ 01-first-project/test-app-1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+        <p>
+          Hello World!
+        </p>
+    </div>
+  );
+}
+
+export default App;
