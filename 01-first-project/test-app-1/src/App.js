8a4cbbe417fab42f26e52729b0be57313b61ade8
Palamarchuk Slava <97830414+slapaic@users.noreply.github.com>
2022-09-07 20:08:38 +0300

    .

--- 01-first-project/test-app-1/src/App.js
+++ 01-first-project/test-app-1/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "./Header";
 import Technologies from "./Technologies";
+import Footer from "./Footer";
 
 const App = () => {
     return (
         <div className="App">
             <Header />
             <Technologies />
+            <Footer />
         </div>
     );
 }
---
+++ 01-first-project/test-app-1/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            test-project-react
+        </div>
+    )
+}
+
+export default Footer;
