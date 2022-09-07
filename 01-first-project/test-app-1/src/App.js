25c651c71c42524df38b5e9cc67a867f1a98bc34
Palamarchuk Slava <97830414+slapaic@users.noreply.github.com>
2022-09-07 18:57:56 +0300

    Lesson 07. Component
    Create test 2 components to App.js

--- 01-first-project/test-app-1/src/App.js
+++ 01-first-project/test-app-1/src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
+const Header = () => {
+    return (
+        <div>
+            <a>Home</a>
+            <a>News Feed</a>
+            <a>Messages</a>
+        </div>
+    )
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+
 const App = () => {
-  return (
-    <div className="App">
-        <p>
-          Hello World!
-        </p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Technologies />
+        </div>
+    );
 }
 
 export default App;
