4c884bc87017868dcad80edad12df7593e51306e
Palamarchuk Slava <97830414+slapaic@users.noreply.github.com>
2022-09-13 13:36:43 +0300

    Lesson 19: routes
    Create routes by react-router-dom for components to Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import {BrowserRouter} from "react-router-dom";
 import './App.css';
 
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Content from "./components/Content/Content";
 
+
 const App = () => {
     return (
-        <div className="App">
-            <Header />
-            <Navbar />
-            <Content />
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <Navbar/>
+                <Content/>
+            </div>
+        </BrowserRouter>
     );
 }
 
--- client/src/components/Content/Content.jsx
+++ client/src/components/Content/Content.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import {Routes, Route} from "react-router-dom";
 import classes from './Content.module.css';
 
 import Profile from "./Profile/Profile";
 import Dialogs from "./Dialogs/Dialogs";
+import News from "./News/News";
+import Music from "./Music/Music";
+import Settings from "./Settings/Settings";
 
 const Content = () => {
     return (
         <div className={classes.content}>
-            <Profile />
-            {/*<Dialogs />*/}
+            <Routes>
+                <Route path="/dialog" element= {<Dialogs/>}/>
+                <Route path="/profile" element={<Profile/>}/>
+                <Route path="/news" element= {<News/>}/>
+                <Route path="/music" element={<Music/>}/>
+                <Route path="/settings" element= {<Settings/>}/>
+            </Routes>
         </div>
     );
 }
