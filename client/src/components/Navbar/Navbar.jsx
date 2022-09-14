43c959901173a4e6132b18d3fedf210de373698f
Palamarchuk Slava <97830414+slapaic@users.noreply.github.com>
2022-09-14 20:32:19 +0300

    Lesson 20: navlink
    add NavLink for href Navbar. fix text decoration

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <a href={"/profile"}>Profile</a>
+                <NavLink to={"/profile"}>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <a href={"/dialog"}>Messages</a>
+                <NavLink to={"/dialog"}>Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <a href={"/news"}>News</a>
+                <NavLink to={"/news"}>News</NavLink>
             </div>
             <div className={classes.item}>
-                <a href={"/music"}>Music</a>
+                <NavLink to={"/music"}>Music</NavLink>
             </div>
             <div className={classes.item}>
-                <a href={"/settings"}>Settings</a>
+                <NavLink to={"/settings"}>Settings</NavLink>
             </div>
         </nav>
     )
