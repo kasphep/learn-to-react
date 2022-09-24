13d48b27ffaed96914215877d39236dd2ed8a7ba
Palamarchuk Slava <97830414+slapaic@users.noreply.github.com>
2022-09-24 15:47:17 +0300

    Lesson 31: addPost button

--- client/src/components/Content/Profile/Posts/AddPost.jsx
+++ client/src/components/Content/Profile/Posts/AddPost.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import classes from './Posts.module.css';
 
+
+const newPostElement = React.createRef();
+
+const addPost = () => {
+    const postText = newPostElement.current.value;
+    alert (postText);
+}
+
 const AddPost = () => {
     return (
         <div className={classes.addPost}>
             <div>
-                <textarea></textarea>
+                <textarea ref = {newPostElement}>
+                    Print text for post
+                </textarea>
             </div>
             <div>
-                <button>Add post</button>
+                <button onClick={addPost}>
+                    Add post
+                </button>
             </div>
         </div>
     )
--- server/Posts/Veshiy/data.js
+++
@@ -1,7 +0,0 @@
-const veshiyPost = {
-    img: './temporaryServer/Posts/Veshiy/img.png',
-    message: 'My first pictures, Veshiy!',
-    likes: '10',
-}
-
-export default veshiyPost;
